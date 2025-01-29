6f10ae9dea15d0841c06cb8226d237f120e9e177
Filippo Galli <galli.filippo@fitstic-edu.com>
2025-01-29 09:49:31 +0100

    fix: remove useless components
    Author: @filgl

--- frontend/railway-frontend/src/app/train-model/train-model.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "app-train-model",
-  imports: [],
-  templateUrl: "./train-model.component.html",
-  styleUrl: "./train-model.component.css",
-})
-export class TrainModelComponent {}
--- frontend/railway-frontend/src/app/train/train.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "app-train",
-  imports: [],
-  templateUrl: "./train.component.html",
-  styleUrl: "./train.component.css",
-})
-export class TrainComponent {}
